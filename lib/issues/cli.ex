99e81de6b7c3ee73bd502163d2ce90da76e66b27
Apoorv2204 <apoorva@uniris.io>
2022-05-15 05:53:26 -0700

    refactor parse_args

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -15,22 +15,26 @@ defmodule Issues.CLI do
   And {github username, project, size(optionall default to 10) }
   Return a tuple of `{username, project, size}`, or `:help`
   if help was given
+
+  two red flags
+  conditonal logic
+  and too long
   """
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+    OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+    |> elem(1)
+    |> args_to_internal_representation()
+  end
 
-    case parse do
-      {[help: true], [], []} ->
-        :help
+  def args_to_internal_representation([user, project, count]) do
+    {user, project, String.to_integer(count)}
+  end
 
-      {_, [username, project, size], _} ->
-        {username, project, String.to_integer(size)}
+  def args_to_internal_representation([user, project]) do
+    {user, project, @default_count}
+  end
 
-      {_, [username, project], _} ->
-        {username, project, @default_count}
-
-      _ ->
-        :help
-    end
+  def args_to_internal_representation(_) do
+    :help
   end
 end
