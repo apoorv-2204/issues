0bf2993584dbdb3dbf9f2dfd96cf10db38a0c705
Apoorv2204 <apoorva@uniris.io>
2022-05-15 05:30:42 -0700

    add Issues.cli.ex module

---
+++ lib/issues/cli.ex
@@ -0,0 +1,36 @@
+defmodule Issues.CLI do
+  @moduledoc """
+  Handling the command line parsing and dispatch to
+  the various functions that end up generating a
+  table of the last _N_  issues in a github project
+  """
+  @default_count 10
+
+  def run(argv) do
+    parse_args(argv)
+  end
+
+  @doc """
+  argv can be [-h or --help, which returns :help]
+  And {github username, project, size(optionall default to 10) }
+  Return a tuple of `{username, project, size}`, or `:help`
+  if help was given
+  """
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+
+    case parse do
+      {[help: true], [], []} ->
+        :help
+
+      {_, [username, project, size], _} ->
+        {username, project, size}
+
+      {_, [username, project], _} ->
+        {username, project, @default_count}
+
+      _ ->
+        :help
+    end
+  end
+end
